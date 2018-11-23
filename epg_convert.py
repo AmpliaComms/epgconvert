0b5f4189dddff6fc4ed714970681af60134c0a27
itamplia <45291744+itamplia@users.noreply.github.com>
2018-11-23 10:27:54 -0400

    Add files via upload

---
+++ epg_convert.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+""" Main program which takes GraceNote ON format files and converts them
+    to TVAnywhere format for Zappware 4.0 as well as the format for Zappware 2.x
+"""
+from datetime import date, timedelta
+import os
+import csv
+import xmltodict
+from epg_event_modules import get_event_data
+from epg_event_modules import create_root
+from epg_event_modules import create_main_tables
+from epg_event_modules import insert_service_info_table
+from epg_event_modules import handle_event
+from epg_event_modules import finally_write_xml_file
+from epg_event_modules import get_service_info
+from epg_event_modules import get_list_of_channels
+from epg_event_modules import get_list_of_events_objects
+from epg_event_modules import write_unique_sort_file
+from zw2_functions import create_root_zw2
+from zw2_functions import write_zw2_channel_info
+
+
+
+
+
+####Start of Program
+if __name__ == "__main__":
+
+#Set up for tranparent running on Windows machines at home and laptop as well as linux
+
+    if os.name == 'nt':
+        if os.getlogin() == 'trevo':#laptop
+            BASEDIRECTORY =\
+                "C:/Users/Trevo/Dropbox/DataToDecisions/MassyComms/Amplia/Gracenote/working"
+        else:
+            if os.getlogin() == 'Dada':#Desktop at home
+                BASEDIRECTORY =\
+                    "C:/Users/Dada/Dropbox/DataToDecisions/MassyComms/Amplia/Gracenote/working"
+    else:#Linux
+        BASEDIRECTORY = "/home/tdeane/TVAnywhere/working"
+
+#setup environment to go through files for catch up as well as going forward if not already in file.
+    NUMBER_FORWARD_DAYS = 0
+    NUMBER_CATCHUP_DAYS = 0
+
+
+#Set up variables to either write xml namespaces and encoding type or not
+#Note by default everything was for Zappware 4 format, if for Zappware 2 then it has zW2
+    CLEANUP_NAMESPACES = False
+    XML_DECLARATION = True
+    ZW2CLEANUP_NAMESPACES = True
+    ZW2XML_DECLARATION = False
+#Change to working directory
+    os.chdir(BASEDIRECTORY)
+#Read genre Cross reference file which matches GraceNote GenreID to TVAnywhere GenreID
+    try :
+        with open('config/graceNoteZappwareGenreMatch.csv', mode='r',
+                  newline=None, encoding="utf-8") as infile:
+            reader = csv.reader(infile)
+            GENREMATCH = {rows[0]:rows[1] for rows in reader}
+            infile.close()
+    except IOError:
+        print(":I/O Error Genre Match file graceNoteZappwareGenreMatch.csv - not found")
+        raise
+#Read list of Amplia Channels from file in config dirctory. It also reads in the 
+#        corresponding callSign as used in Zappware 2.x epg files in order to be able
+#        to continue using the same ZW2 call signs in the zw2 epg xml files created.
+    try:
+        with open('config/AmpliaChannels.csv', mode='r', newline=None, encoding="utf-8") as infile:
+            reader = csv.reader(infile)
+            amplia_channels = {rows[0]:rows[1] for rows in reader}
+            infile.close()
+    except IOError:
+        print(":I/O Error Genre Match file AmpliaChannels.csv - not found")
+        raise
+#set reference day
+    reference_day = date.today()
+#will need logic to select source file for day
+    SourceFilename = 'graceNoteInputFiles/on_car_samp_tv_sources_v22_current.xml'
+#set up array of days from - catchup days to + number of forward days in EPG
+    day_number = -NUMBER_CATCHUP_DAYS
+    list_of_days = []
+    while day_number <= NUMBER_FORWARD_DAYS:
+        list_of_days.append(reference_day + timedelta(days=day_number))
+        day_number = day_number + 1
+
+    for schedule_day in list_of_days:
+#For each day get a list of channels in the source file for that day
+
+        channel_list_gn = get_list_of_channels(schedule_day)
+# this variable has a list of the channels to be processed as read from config file
+        channel_list = amplia_channels.keys()
+#  On first day through here we need to open for writing
+#     the file for the effective day and channel
+# Using the objectify library to create the xml files
+
+#    we need to create the main static tables in the objectify object
+#   we also need to write the service information table at this time
+#    insertServieInformationTable(channel)
+
+
+        for channel_element in channel_list:
+            if channel_element in channel_list_gn:
+#Get list of all the events for that channel for that day
+                gn_events_for_channel_day_xml = get_list_of_events_objects(channel_element)
+    #transform the xml retrieved into a dict structure with all the tags and attributes
+                gn_events_for_channel_day = xmltodict.parse(gn_events_for_channel_day_xml)
+                print(channel_element)    #print channel that we are working on
+    #    we need to create the root of the objectify object for both Zappware 4 file and Zappware 2 file
+                root_of_objectivy_object = create_root()
+                root_zw2 = create_root_zw2()
+    #start forming an array structure for the xml objects
+                ProgramInformationArray = []
+    #Get service information info for that channel
+                service_info = get_service_info(channel_element, SourceFilename)
+    #  we need to create the main static tables in the objectify object
+                main_tables = create_main_tables(root_of_objectivy_object, channel_element)
+    # write the ServiceInformationTable (ZW4)
+                insert_service_info_table(main_tables["service_information_table"], service_info)
+    #write the ChannelInformation (ZW2)
+                write_zw2_channel_info(root_zw2, service_info, amplia_channels)
+    #set up file names for the output files.
+                filename_xml = 'zappware4Output/xml/' +channel_element +\
+                    "_" + reference_day.strftime('%Y%m%d') +'.xml'
+                filename_zw2_xml = 'zappware2Output/xml/' + amplia_channels[service_info["serviceId"]] +\
+                    "_" + reference_day.strftime('%Y%m%d') +'.xml'
+                filename_img = 'zappware4Output/images/' +channel_element +\
+                    "_" + reference_day.strftime('%Y%m%d') +'.txt'
+                filename_zw2_img = 'zappware2Output/images/' + amplia_channels[service_info["serviceId"]] +\
+                    "_" + reference_day.strftime('%Y%m%d') +'.txt'
+    #set up variables to hold programIDs and groupIDs used, as well as graphic assets needed.
+                program_ids_already_used = []
+                group_ids_already_used = []
+                graphics_assets = []
+                zw2_graphics_assets = []
+                program_information_index = 0
+    #For each event on the day
+                for element in gn_events_for_channel_day['xml']['event']:
+    #for each event on the channel for the day get event data into data structure from xml file
+                    event_data = get_event_data(element)
+#                    print(event_data['program_id']) #print programId being handled
+    
+                    handle_event(main_tables, event_data, program_ids_already_used,
+                                 group_ids_already_used, GENREMATCH, root_zw2,
+                                 service_info, graphics_assets, zw2_graphics_assets,
+                                 amplia_channels)
+                #PLACE TO CALL FUNCTION TO WRITE FOR SINGLE EVENT
+                write_unique_sort_file(filename_img, graphics_assets, BASEDIRECTORY)
+                write_unique_sort_file(filename_zw2_img, zw2_graphics_assets, BASEDIRECTORY)
+                finally_write_xml_file(root_of_objectivy_object, filename_xml,
+                                       BASEDIRECTORY, CLEANUP_NAMESPACES, XML_DECLARATION)
+                finally_write_xml_file(root_zw2, filename_zw2_xml, BASEDIRECTORY,
+                                       ZW2CLEANUP_NAMESPACES, ZW2XML_DECLARATION)
+            else:
+                print("Channel - "+ channel_element+" Not in Gracenote repository")
+    
+        #        del eventRating
+        #        del eventRatingBody
+        #        del tvquals
+        #        del netSynSourceForEvent
+        #        del netSynTypeForEvent
---
+++ zw2_functions.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 12:15:16 2018
+
+@author: Trevor Deane
+"""
+from lxml import objectify
+#Zappware 2x specific functions
+
+def create_root_zw2():
+    """This function generates the lxml root element for the Zappware 2 xml tree
+    It  includes all of the necessary namespaces and returns the root element
+    """
+    root_zw2 = objectify.Element('tv')
+    root_zw2.set('{http://www.w3.org/XML/1998/namespace}lang', 'EN')
+
+    return root_zw2
+
+
+def get_zw2_channel_data(service_info, amplia_channels):
+    """This function takes the service Information dict variable created for ZW4
+     and returns relevant channel information in a list variable
+    """
+    zw2_channel_data = dict()
+    zw2_channel_data["channelId"] = service_info["serviceId"]
+    zw2_channel_data["displayName"] = service_info["name"]
+    if isinstance(service_info["lang"], list):
+        zw2_channel_data["displayNameLang"] = service_info["lang"][0]
+    else:
+        zw2_channel_data["displayNameLang"] = service_info["lang"]
+    zw2_channel_data["callSign"] = service_info["callSign"]
+    zw2_channel_data["callSign_amplia"] = amplia_channels[service_info["serviceId"]]
+    zw2_channel_data["channelUrl"] = ""
+    return zw2_channel_data
+
+def write_zw2_channel_info(root_zw2, service_info, amplia_channels):
+    """This function takes the Zappware 2 lxml root variable and the specific
+    ZW2 service Information dict variable and adds the channel data to the
+    lxml object
+    """
+    zw2_channel_data = get_zw2_channel_data(service_info, amplia_channels)
+    channel_tag = objectify.Element("channel")
+    root_zw2.append(channel_tag)
+    channel_tag.set("id", zw2_channel_data["callSign_amplia"])
+    display_name = objectify.SubElement(channel_tag, "display-name")
+    display_name._setText(zw2_channel_data["displayName"])
+    display_name.set("lang", zw2_channel_data["displayNameLang"])
+    channel_tag.Url = zw2_channel_data["channelUrl"]
+    return
+
+
+def get_zw2_image_data(grace_note_dict):
+    """This function takes the xmltodict list variable which was created from the
+    xml data for the specific programID and checks for images which are
+    360x270px and Iconic and creates and returns a dict variable with those images
+    references
+    """
+    zw2_image_data = dict()
+    zw2_image_data['imageSource'] = ''
+    zw2_image_data['type'] = ''
+    try:
+        if isinstance(grace_note_dict['xml']['assets']['asset'], list):
+            for element in grace_note_dict['xml']['assets']['asset']:
+                if element['@width'] == '360' and element['@height'] == '270'\
+                and element['@category'] == 'Iconic':
+                    zw2_image_data['imageSource'] = element['URI'][7:]
+                    zw2_image_data['imageSourceWithDirectory'] = element['URI']
+                    zw2_image_data['type'] = '99'
+                    return  zw2_image_data
+        else:
+            if element['@width'] == '360' and element['@height'] == '270'\
+            and element['@category'] == 'Iconic':
+                zw2_image_data['imageSource'] = element['URI'][7:]
+                zw2_image_data['imageSourceWithDirectory'] = element['URI']
+                zw2_image_data['type'] = '99'
+                return zw2_image_data
+        return zw2_image_data
+
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        return zw2_image_data
+
+
+def get_zw2_programme_data(b_d_unique, service_info, event_data, grace_note_dict, b_d_synopsis,
+                           amplia_channels):
+    """This function takes the specific ZW4 dict variables with relevant data
+    as well as the xmltodict list variable which was created from the
+    xml data for the specific programID returns a dict variable with data required
+    to complete the Zappware 2.x programme Data table
+    """
+    zw2_programme_data = dict()
+    zw2_programme_data["startTime"] = event_data['gmt_date_time_start_zw2']
+    zw2_programme_data["stopTime"] = event_data['gmt_date_time_end_zw2']
+    zw2_programme_data["channel"] = service_info["name"]
+    zw2_programme_data["callSign"] = service_info["callSign"]
+    zw2_programme_data["callSign_amplia"] = amplia_channels[service_info["serviceId"]]
+    zw2_programme_data["recordable"] = "y"
+    zw2_programme_data["npvrenable"] = "y"
+    zw2_programme_data["id"] = b_d_unique['program_id']+'.'+\
+            event_data['gmt_date_time_start_zw2'][:-6] + '.' +service_info["serviceId"]
+    zw2_programme_data["type"] = b_d_unique['BDKeyword']
+    zw2_programme_data["programTitle"] = b_d_unique['BDTitle']
+    zw2_programme_data["language"] = b_d_unique['BDTitle_att_lang']
+    try:
+        zw2_programme_data["programSubTitle"] =\
+            grace_note_dict['xml']['episodeInfo']['title']['#text']
+        zw2_programme_data["subTitleLanguage"] =\
+            grace_note_dict['xml']['episodeInfo']['title']['@lang']
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        zw2_programme_data["programSubTitle"] = ""
+        zw2_programme_data["subTitleLanguage"] = ""
+#    b_d_synopsis = createbDSynopsis(grace_note_dict)
+    try:
+        zw2_programme_data["desc"] = b_d_synopsis[0]["b_d_synopsis"]
+        zw2_programme_data["descLanguage"] = b_d_synopsis[0]["b_d_synopsis_att_lang"]
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        zw2_programme_data["desc"] = ''
+        zw2_programme_data["descLanguage"] = ''
+    zw2_image_data = get_zw2_image_data(grace_note_dict)
+    try:
+        zw2_programme_data["imageSource"] = zw2_image_data['imageSource']
+        zw2_programme_data["imageSourceWithDirectory"] = zw2_image_data['imageSourceWithDirectory']
+        zw2_programme_data["imageType"] = zw2_image_data['type']
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        zw2_programme_data["imageSource"] = ''
+        zw2_programme_data["imageSourceWithDirectory"] = ''
+        zw2_programme_data["imageType"] = ''
+    zw2_programme_data["country"] = b_d_unique['BDProductionLocation']
+    try:
+        zw2_programme_data["seasonNumber"] = int(grace_note_dict['xml']['episodeInfo']['@season'])
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        zw2_programme_data["seasonNumber"] = ""
+    try:
+        zw2_programme_data["episodeNumber"] = int(grace_note_dict['xml']['episodeInfo']['@number'])
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        zw2_programme_data["episodeNumber"] = ""
+    if zw2_programme_data["episodeNumber"] != "":
+        if zw2_programme_data["seasonNumber"] != "":
+            zw2_programme_data["episodeNum"] = str(zw2_programme_data["seasonNumber"] - 1) +\
+                '.' + str(zw2_programme_data["episodeNumber"] -  1) + '.0'
+        else:
+            zw2_programme_data["episodeNum"] = '0' +\
+                    '.' + str(zw2_programme_data["episodeNumber"] -  1) + '.0'
+    else:
+        zw2_programme_data["episodeNum"] = ""
+    zw2_programme_data["episodeSystem"] = "xmltv_ns"
+    zw2_programme_data["ratingSystem"] = "DVB"
+    zw2_programme_data["minimumAge"] = b_d_unique['BDParentalGuidanceMinimumAge']
+    zw2_programme_data["key"] = 'releaseStatus'
+    if event_data['quals'].find('New') >= 0:
+        zw2_programme_data["value"] = "0"
+    else:
+        zw2_programme_data["value"] = "1"
+    return zw2_programme_data
+
+
+def write_zw2_programme_info(root_zw2, event_data, b_d_unique, service_info, grace_note_dict,
+                             b_d_credits, b_d_synopsis, b_d_genres, zw2_graphics_assets,
+                             amplia_channels):
+    """This function takes the lxml objectify root object for Zappware 2.x,
+    the xmltodict list variable which was created from the xml data for the specific
+    programID, various specific ZW4 dict variables with relevant data and the
+    zappware 2 variable which holds the running list of the graphics assets
+    used and adds the relevant objects to the lxml objectify programme Table. It
+    also appends any new images used to the graphics assets used variable
+    """
+    zw2_programme_data = get_zw2_programme_data(b_d_unique, service_info, event_data,
+                                                grace_note_dict, b_d_synopsis,
+                                                amplia_channels)
+    programme_tag = objectify.Element("programme")
+    root_zw2.append(programme_tag)
+    programme_tag.set("start", zw2_programme_data["startTime"])
+    programme_tag.set("stop", zw2_programme_data["stopTime"])
+    programme_tag.set("channel", zw2_programme_data["callSign_amplia"])
+    programme_tag.set("recordable", zw2_programme_data["recordable"])
+    programme_tag.set("npvrenable", zw2_programme_data["npvrenable"])
+    programme_tag.set("id", zw2_programme_data["id"])
+    if zw2_programme_data["id"][0:2] == 'SH':
+        programme_tag.set("type", 'program')
+    elif zw2_programme_data["id"][0:2] == 'EP':
+        programme_tag.set("type", 'episode')
+    else:
+        programme_tag.set("type", '')
+#    programme_tag.set("type", zw2_programme_data["type"])
+    programme_tag.title = zw2_programme_data["programTitle"]
+    programme_tag.title.set("lang", zw2_programme_data["language"])
+    if zw2_programme_data["programSubTitle"] != '':
+        programme_tag.subTitle = zw2_programme_data["programSubTitle"]
+        programme_tag.subTitle.set("lang", zw2_programme_data["subTitleLanguage"])
+    programme_tag.desc = zw2_programme_data["desc"]
+    programme_tag.desc.set("lang", zw2_programme_data["descLanguage"])
+#multiple credits
+    programme_tag.credits = ""
+#    b_d_credits = createbDCredits(grace_note_dict)
+    try:
+        for b_d_credits_element in b_d_credits:
+            if b_d_credits_element["BDPersonNameRole"].upper() == "PRODUCER":
+                objectify.SubElement(programme_tag.credits, 'producer').\
+                    _setText(b_d_credits_element["BDPersonNameGivenName"] +\
+                             ' ' + b_d_credits_element["BDPersonNameFamilyName"])
+            elif b_d_credits_element["BDPersonNameRole"].upper() == "DIRECTOR":
+                objectify.SubElement(programme_tag.credits, 'director').\
+                    _setText(b_d_credits_element["BDPersonNameGivenName"] +\
+                             ' ' + b_d_credits_element["BDPersonNameFamilyName"])
+            elif b_d_credits_element["BDPersonNameRole"].upper() == "ACTOR":
+                objectify.SubElement(programme_tag.credits, ' actor').\
+                    _setText(b_d_credits_element["BDPersonNameGivenName"] +\
+                             ' ' + b_d_credits_element["BDPersonNameFamilyName"])
+            elif b_d_credits_element["BDPersonNameRole"].upper() == "WRITER":
+                objectify.SubElement(programme_tag.credits, 'writer').\
+                    _setText(b_d_credits_element["BDPersonNameGivenName"] +\
+                             ' ' + b_d_credits_element["BDPersonNameFamilyName"])
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        pass
+#    programme_tag.credits.date = "date"
+#multiple categories
+#    bDGeneres = createbDGenre(grace_note_dict, GENREMATCH)
+    try:
+        for b_d_generes_element in b_d_genres:
+            temp_obj = objectify.SubElement(programme_tag, 'category')
+            temp_obj._setText(b_d_generes_element['genreName'])
+    #        programme_tag.category = "Category to put here"
+            temp_obj.set("lang", "Language")
+    except (UnboundLocalError, KeyError, IndexError, LookupError, NameError, ValueError):
+        pass
+
+    programme_tag.icon = zw2_programme_data["imageSource"]
+    zw2_graphics_assets.append(zw2_programme_data["imageSourceWithDirectory"])
+    programme_tag.icon.set("ptype", zw2_programme_data["imageType"])
+    programme_tag.country = zw2_programme_data["country"]
+    episode_num = objectify.SubElement(programme_tag, "episode-num")
+
+    episode_num._setText(zw2_programme_data["episodeNum"])
+    episode_num.set("system", zw2_programme_data["episodeSystem"])
+    programme_tag.rating = ""
+    programme_tag.rating.set("system", zw2_programme_data["ratingSystem"])
+    programme_tag.rating.value = zw2_programme_data["minimumAge"]
+    programme_tag.extentionInfo = ""
+    programme_tag.extentionInfo.key = zw2_programme_data["key"]
+    programme_tag.extentionInfo.value = zw2_programme_data["value"]
+    return
